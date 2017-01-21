09ab7f6183d1e77854cea902eb4cead68505829b
Jennifer Louise  Doig <jenn@Jennifers-iMac.attlocal.net>
2017-01-21 00:54:00 -0500

    Second commit. Added js file. Added new logo. Added contact links.

---
+++ main.js
@@ -0,0 +1,9 @@
+//give me a title program
+
+function newTitle(){
+  var yourName = document.getElementById("username");
+  var name = username.value;
+  alert("Supreme Leader of the Universe " + name + "!")
+ }
+
+
